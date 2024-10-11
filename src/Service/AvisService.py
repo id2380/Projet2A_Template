29db7e50f226e84cf677e2890d02afcda5ab339e
Gabrielle Joron <gabrielle.joron@eleve.ensai.fr>
2024-10-11 17:01:05 +0200

    AvisService gabrielle

---
+++ src/Service/AvisService.py
@@ -0,0 +1,21 @@
+#importer les modules nécéssaires
+
+class AvisService :
+    def __init__(
+
+    )
+
+    def consulter_note_moyenne(self, film : Film) :
+        pass 
+
+    def ajouter_avis(self, film : Film) :
+        pass 
+
+    def ajouter_avis(self, avis : Avis) :
+        pass 
+
+    def modifier_avis(self, avis : Avis) :
+        pass
+
+    def supprimer_avis(self, avis : Avis) :
+        pass
