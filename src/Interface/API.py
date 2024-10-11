0d2a61e82ed6df07186b81f0568862799bcaded5
Jules Robin <id2678@eleve.ensai.fr>
2024-10-11 15:45:48 +0200

    Suppression Jules

--- src/Interface/API.py
+++ src/Interface/API.py
@@ -12,17 +12,6 @@ def run_app(movie_service: MovieService):
     def read_root():
         return {"Hello": "World"}
 
-    @app.get("/movies/{tmdb_id}", status_code=status.HTTP_200_OK)
-    def get_movie_by_id(tmdb_id: int) -> Movie:
-        try:
-            my_movie: Movie = movie_service.get_by_id(tmdb_id)
-            return my_movie
-        except FileNotFoundError:
-            raise HTTPException(
-                status_code=404,
-                detail="Movie with id [{}] not found".format(tmdb_id),
-            ) from FileNotFoundError
-        except Exception:
-            raise HTTPException(status_code=400, detail="Invalid request") from Exception
+    
 
     uvicorn.run(app, port=8000, host="localhost")
