2a49b51fa570ea728736fb23f884a022f56f6d10
Jules Robin <id2678@eleve.ensai.fr>
2024-10-17 15:41:18 +0200

    Ajout de la classe film_client

---
+++ src/Client/film_client.py
@@ -0,0 +1,48 @@
+import json
+import os
+
+import requests
+
+from src.business_object.film import Film
+from src.utils.singleton import Singleton
+
+
+class FilmClient(metaclass=Singleton):
+    def __init__(self) -> None:
+        self.__HOST = os.environ["WEBSERVICE_HOST"]
+        self._headers = {"accept": "application/json",
+                         "Authorization": os.environ["WEBSERVICE_TOKEN"]}
+
+    def search_movies(self, page: int = 1, language: str = "en-US"):
+        url = f"{self.__HOST}/discover/movie"
+        params = {"include_adult": False,
+                  "language": language,
+                  "include_video": False,
+                  "page": page,
+                  "sort_by": "popularity.desc"}
+        req = requests.get(url, headers=self._headers, params=params)
+
+        # GENRE A COMPLETER !!!!!!
+        films = []
+        if req.status_code == 200:
+            raw_films = req.json()["results"]
+            for raw_film in raw_films:
+                film = Film(id_film=raw_film["id"],
+                            titre=raw_film["title"],
+                            genre="",
+                            date_de_sortie=raw_film["release_date"],
+                            langue_originale=raw_film["original_language"],
+                            synopsis=raw_film["overview"])             
+                if film:
+                    films.append(film)
+        return films
+
+# Pour tester à supprimer par la suite
+if __name__ == "__main__":
+    # Pour charger les variables d'environnement contenues dans le fichier .env
+    import dotenv
+    dotenv.load_dotenv(override=True)
+
+    filmClient = FilmClient()
+
+    filmClient.search_movies()
