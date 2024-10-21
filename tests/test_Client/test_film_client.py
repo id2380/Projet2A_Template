6f0656ae7bdf4992b5d77ad30e1ba2e0361b8c58
FannyMoreau <fanny.moreau@eleve.ensai.fr>
2024-10-21 16:05:41 +0200

    film_client

--- tests/test_Client/test_film_client.py
+++ tests/test_Client/test_film_client.py
@@ -7,8 +7,9 @@ from pydantic_core import ValidationError
 from src.client.film_client import FilmClient
 
 
-@mock.patch.dict(os.environ, {"WEBSERVICE_HOST": "https://api.themoviedb.org/3","WEBSERVICE_TOKEN":"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTJhMmU0OTUyOTcwZDBmNWQ4ZTZiMjI2ZmFmZGMwNCIsIm5iZiI6MTcyNjgyMDY4My4zMDYxMjYsInN1YiI6IjY2ZWQyYzNiY2RkMTA4ZWQ5MzIyYWYzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2vVcbaxM_PX11RFgys6jhTskiwiV1t0fCChn8K0Hlxs"})
+@mock.patch.dict(os.environ, {"WEBSERVICE_HOST": "https://api.themoviedb.org/3", "WEBSERVICE_TOKEN": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTJhMmU0OTUyOTcwZDBmNWQ4ZTZiMjI2ZmFmZGMwNCIsIm5iZiI6MTcyNjgyMDY4My4zMDYxMjYsInN1YiI6IjY2ZWQyYzNiY2RkMTA4ZWQ5MzIyYWYzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2vVcbaxM_PX11RFgys6jhTskiwiV1t0fCChn8K0Hlxs"})
 class TestFilmClient:
+    # methode search_movies
     def test_search_movies_ok(self):
         # GIVEN
         film_client = FilmClient()
@@ -39,7 +40,29 @@ class TestFilmClient:
         # THEN
         assert films is None
 
+    # methode getSimilarMovies
+
+    def test_getSimilarMovies_ok(self, movieId=1184918):
+        # GIVEN
+        film_client = FilmClient()
+
+        # WHEN
+        films = film_client.getSimilarMovies(movieId)
+
+        # THEN
+        assert films is not None
+
+    def test_getSimilarMovies_error(self, movieId=0):
+        # GIVEN
+        film_client = FilmClient()
+
+        # WHEN
+        films = film_client.getSimilarMovies(movieId)
+
+        # THEN
+        assert films is None
+
+
 if __name__ == "__main__":
     import pytest
-
     pytest.main([__file__])
