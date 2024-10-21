0fe88dfd38c71742a1c1428f2a352d1dd4da75c0
Jules Robin <id2678@eleve.ensai.fr>
2024-10-21 16:04:38 +0200

    Ajout de film dao

---
+++ tests/test_dao/test_film_dao.py
@@ -0,0 +1,31 @@
+import time
+from datetime import datetime
+
+from src.business_object.film import Film
+from src.dao.film_dao import FilmDAO
+
+
+class TestFilmDao:
+    def test_create_film_ok(self):
+        # GIVEN
+        film_dao = FilmDAO()
+        film = Film(
+            id_film=1,
+            titre="Test",
+            genre="Test",
+            date_de_sortie=datetime(2024, 10, 21),
+            langue_originale="Français",
+            synopsis="Ceci est un test.",
+        )
+        # WHEN
+        created = film_dao.creer_film(film)
+
+        # THEN
+        assert created
+
+    
+
+if __name__ == "__main__":
+    import pytest
+
+    pytest.main([__file__])
