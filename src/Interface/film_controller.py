00b9a6c4e873458cc90bc8ef254a261b735b6f6a
Jules Robin <id2678@eleve.ensai.fr>
2024-10-23 10:51:59 +0200

    Ajout endpoint fiche technique

--- src/Interface/film_controller.py
+++ src/Interface/film_controller.py
@@ -10,6 +10,7 @@ from src.Interface.jwt_bearer import JWTBearer
 from src.Service.mot_de_passe_service import valider_nom_utilisateur_mot_de_passe
 from pydantic import BaseModel
 from src.Service.film_service import FilmService
+from src.Service.avis_service import AvisService
 
 if TYPE_CHECKING:
     from src.Model.Movie import Movie
@@ -37,15 +38,32 @@ def recherche_films(title: str = None,
         raise HTTPException(status_code=404, detail=f"Erreur : {str(e)}")
 
 @film_router.get("/recherche_films_similaires", status_code=status.HTTP_200_OK)
-def recherche_films_similaires(id_film: int, language: str = "en-US"):
+def recherche_films_similaires(id_film: int):
     import dotenv
     dotenv.load_dotenv(override=True)
     films = None
     try:
-        films = FilmService().recherche_films_similaires(id_film, language)
+        films = FilmService().recherche_films_similaires(id_film)
         # Vérification si l'avis a été créé avec succès
         if films is None:
             raise ValueError("Aucun film ne correspond à vos critères")
         return films
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=f"Erreur : {str(e)}")
+
+@film_router.get("/fiche_technique", status_code=status.HTTP_200_OK)
+def obtenir_fiche_technique(id_film: int):
+    import dotenv
+    dotenv.load_dotenv(override=True)
+    try:
+        film = FilmService().obtenir_film_complet(id_film)
+        if film is None:
+            raise ValueError("Aucun film ne correspond à l'ID")
+        """
+        avis_service = AvisService()
+        film.fiche_technique.note_moyenne = avis_service.calculer_note_moyenne(id_film)
+        film.fiche_technique.avis = avis_service.obtenir_avis_par_film(id_film)
+        """
+        return film
     except Exception as e:
         raise HTTPException(status_code=404, detail=f"Erreur : {str(e)}")
