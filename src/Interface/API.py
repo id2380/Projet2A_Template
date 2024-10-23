783f31eaa1a434f47de7475e0187cb6ba95a4124
Jules Robin <id2678@eleve.ensai.fr>
2024-10-23 10:28:45 +0200

    Ajout endpoint

--- src/Interface/API.py
+++ src/Interface/API.py
@@ -4,11 +4,14 @@ import uvicorn
 from fastapi import FastAPI
 
 from src.Interface.user_controller import user_router
+from src.Interface.film_controller import film_router
 
 
 def run_app():
     app = FastAPI(title="Cinégram", description="Application de gestion de films")
 
     app.include_router(user_router)
+
+    app.include_router(film_router)
 
     uvicorn.run(app, port=8000, host="localhost")
