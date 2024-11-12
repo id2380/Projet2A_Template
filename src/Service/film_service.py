ebd76a352e2370cd5526f1fdb3484683df4b1a87
Jules Robin <id2678@eleve.ensai.fr>
2024-11-12 15:38:06 +0100

    Commentaires de la classe FilmService

--- src/Service/film_service.py
+++ src/Service/film_service.py
@@ -67,13 +67,54 @@ class FilmService:
             raise ValueError("Aucun film ne correspond à vos critères.")
         return films
 
+    """
+    Permet de rechercher une liste de films dans l'API Tmdb qui sont similaires
+    au film passé en paramètre. 
+
+    Paramètres
+    ----------
+    id_film : int
+        L'identifiant du film.
+    page : int
+        Le nombre de pages de films à charger dans l'API.
+    language : str
+        La langue utilisée dans l'API, par défaut c'est le français.
+
+    Retour
+    ----------
+    list[Film] : liste de films similaires.
+
+    Exception
+    ----------
+    valueError : erreur de communication avec l'API.
+    valueError : identifiant du film non valide.
+    """
     def recherche_films_similaires(self,
                                    id_film: int,
-                                   language: str = "en-US",
+                                   language: str = "fr",
                                    page: int = 1):
         films = FilmClient().obtenir_films_similaires(id_film, language, page)
         return films
 
+    """
+    Permet de rechercher un film à partir de son identifiant. Si il existe déjà
+    dans la base, les informations sont recherchées dans la base, sinon elles sont
+    recherchées sur l'API Tmdb.
+
+    Paramètres
+    ----------
+    id_film : int
+        L'identifiant du film.
+
+    Retour
+    ----------
+    Film_Complet : Le film recherché.
+
+    Exception
+    ----------
+    valueError : erreur de communication avec l'API.
+    valueError : identifiant du film non valide.
+    """
     def recherche_film_id(self, id_film: int):
         film_dao = FilmDAO()
         if film_dao.existe_film(id_film):
@@ -83,6 +124,21 @@ class FilmService:
             film_dao.creer_film(film)
         return film
 
+    """
+    Permet de créer un film dans la base de données. Si il existe déjà
+    dans la base, une erreur est renvoyé, sinon il est crée.
+
+    Paramètres
+    ----------
+    id_film : int
+        L'identifiant du film.
+
+    Exception
+    ----------
+    valueError : Erreur de communication avec l'API.
+    valueError : L'identifiant du film  est non valide.
+    valueError : Le film existe déjà dans la base.
+    """
     def creer_film(self, id_film: int):
         film_dao = FilmDAO()
         if film_dao.existe_film(id_film):
@@ -91,6 +147,21 @@ class FilmService:
             film = FilmClient().recherche_film_id(id_film)
             film_dao.creer_film(film)
 
+    """
+    Permet de supprimer un film de la base de données. Si il existe
+    dans la base, il est supprimé, sinon une erreur est renvoyée.
+
+    Paramètres
+    ----------
+    id_film : int
+        L'identifiant du film.
+
+    Exception
+    ----------
+    valueError : Erreur de communication avec l'API.
+    valueError : L'identifiant du film  est non valide.
+    valueError : Le film n'existe pas dans la base.
+    """
     def supprimer_film(self, id_film: int):
         film_dao = FilmDAO()
         if not film_dao.existe_film(id_film):
