7c9027abc3a7ebd5b96318f2c49ba51df57a43ba
Jules Robin <id2678@eleve.ensai.fr>
2024-11-12 13:58:35 +0100

    Modification de la classe FilmService

--- src/Service/film_service.py
+++ src/Service/film_service.py
@@ -3,6 +3,44 @@ from src.dao.film_dao import FilmDAO
 
 
 class FilmService:
+    """
+    Classe contenant les méthodes de service pour les films.
+    """
+
+    # -------------------------------------------------------------------------
+    # Méthodes
+    # -------------------------------------------------------------------------
+
+    """
+    Permet de rechercher une liste de films dans l'API Tmdb. Utilise la bonne
+    fonction de recherche dans la classe FilmClient en fonction des critères
+    de recherche.
+
+    Paramètres
+    ----------
+    titre : str
+        La chaîne de caractères qui doit être contenue dans le titre.
+    page : int
+        Le nombre de pages de films à charger dans l'API.
+    language : str
+        La langue utilisée dans l'API, par défaut c'est le français.
+    primary_release_year : int
+        L'année de première sortie du film.
+    region : str
+        La région où le film doit être disponible.
+    year : int
+        L'année de production du film.
+
+    Retour
+    ----------
+    list[Film] : liste de films populaires remplissant les caractéristiques
+    en paramètres
+
+    Exception
+    ----------
+    ValueError : en cas de problème de communication avec l'API.
+    ValueError : si aucun film ne correspond aux critères de la recherche.
+    """
     def recherche_films(
         self,
         title: str = None,
@@ -14,41 +52,48 @@ class FilmService:
     ):
         if title is None:
             films = FilmClient().recherche_films(page,
-                                                    language,
-                                                    primary_release_year,
-                                                    region,
-                                                    year)
+                                                 language,
+                                                 primary_release_year,
+                                                 region,
+                                                 year)
         else:
             films = FilmClient().recherche_films_titre(title,
-                                                        page,
-                                                        language,
-                                                        primary_release_year,
-                                                        region,
-                                                        year)
+                                                       page,
+                                                       language,
+                                                       primary_release_year,
+                                                       region,
+                                                       year)
         if len(films) == 0:
             raise ValueError("Aucun film ne correspond à vos critères.")
         return films
 
-    def creer_film(self, id_film: int):
-        Film = FilmClient().recherche_film_id(id_film)
-        if Film is not None:
-            boolean = FilmDAO().creer_film(Film)
-            return boolean
-        return False
-
-    def recherche_films_similaires(self, id_film: int, language: str = "en-US", page: int = 1):
+    def recherche_films_similaires(self,
+                                   id_film: int,
+                                   language: str = "en-US",
+                                   page: int = 1):
         films = FilmClient().obtenir_films_similaires(id_film, language, page)
         return films
 
-    def obtenir_film_complet(self, id_film: int):
-        film_complet = FilmClient().recherche_film_id(id_film)
-        return film_complet
+    def recherche_film_id(self, id_film: int):
+        film_dao = FilmDAO()
+        if film_dao.existe_film(id_film):
+            film = film_dao.lire_film(id_film)
+        else:
+            film = FilmClient().recherche_film_id(id_film)
+            film_dao.creer_film(film)
+        return film
 
+    def creer_film(self, id_film: int):
+        film_dao = FilmDAO()
+        if film_dao.existe_film(id_film):
+            raise ValueError("Le film existe déjà.")
+        else:
+            film = FilmClient().recherche_film_id(id_film)
+            film_dao.creer_film(film)
 
-if __name__ == "__main__":
-    import dotenv
-
-    dotenv.load_dotenv(override=True)
-
-    film_service = FilmService()
-    print(film_service.creer_film(1184918))
+    def supprimer_film(self, id_film: int):
+        film_dao = FilmDAO()
+        if not film_dao.existe_film(id_film):
+            raise ValueError("Le film n'existe pas.")
+        else:
+            film_dao.supprimer_film(id_film)
