963d93e73dedd5aebed1745755c0c2ccaaa4f9c6
id2380 <thibaut.louza@eleve.ensai.fr>
2024-11-18 16:17:05 +0100

    Modification des tests sur l'encodage et le décodage de jeton pour tenir compte de la nouvelle durée

--- tests/test_service/test_JWTService.py
+++ tests/test_service/test_JWTService.py
@@ -11,17 +11,18 @@ jwt_service = JwtService("mysecret")
 def test_encode_jwt():
     user_id = "myUser"
     jwtResponse = jwt_service.encode_jwt(user_id=user_id)
+    print(jwtResponse.access_token)
     assert (
         jwtResponse.access_token
-        == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibXlVc2VyIiwiZXhwaXJ5X3RpbWVzdGFtcCI6MTcyNDY3NDIwMC4wfQ.eUjNbpMCDNuPESsMHF2dpeRlDl6fMJmjUWsHSZT_n1Q"  # noqa: E501
-    )
+        == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibXlVc2VyIiwiZXhwaXJ5X3RpbWVzdGFtcCI6MTcyNDY3NTQwMC4wfQ.HGQSpvCYTD3h7H9_Ys--ReQabvdOsdikeofkMHtHYMc"
 
+    )
 
 @freeze_time("2024-08-26 12:00:00")
 def test_decode_jwt():
-    jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibXlVc2VyIiwiZXhwaXJ5X3RpbWVzdGFtcCI6MTcyNDY3NDIwMC4wfQ.eUjNbpMCDNuPESsMHF2dpeRlDl6fMJmjUWsHSZT_n1Q"  # noqa: E501
+    jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibXlVc2VyIiwiZXhwaXJ5X3RpbWVzdGFtcCI6MTcyNDY3NTQwMC4wfQ.HGQSpvCYTD3h7H9_Ys--ReQabvdOsdikeofkMHtHYMc"  
     decoded_jwt = jwt_service.decode_jwt(jwt)
     assert decoded_jwt.get("user_id") == "myUser"
     assert datetime.datetime.fromtimestamp(decoded_jwt.get("expiry_timestamp")) == datetime.datetime.fromisoformat(
-        "2024-08-26 12:10:00"
+        "2024-08-26 12:30:00"  # Expiration après 30 minutes
     )
